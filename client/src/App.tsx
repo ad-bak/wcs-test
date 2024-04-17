6409ee32bdc76460714fa633be3fa31e22ec832e
Adnan <adbak.dev@gmail.com>
2024-04-17 15:26:00 +0200

    webhooks test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,18 +16,20 @@ const ALL_WILDERS = gql`
 `;
 
 export type WilderData = {
-  _id: string
-  name:string
-  city: string
-}
+  _id: string;
+  name: string;
+  city: string;
+};
 
 function App() {
-  
-  const { loading, error, data } = useQuery<{wilders:WilderData[]}>(ALL_WILDERS);
+  const { loading, error, data } = useQuery<{ wilders: WilderData[] }>(
+    ALL_WILDERS
+  );
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
   return (
     <div>
+      <p>test 17/04/2024</p>
       <Header>
         <Container>
           <h1>Wilders Book with CD</h1>
